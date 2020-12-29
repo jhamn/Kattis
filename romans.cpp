2bae8b3a09dbdce95bb613e3fcaffca2f770e035
jhamn <36033772+jhamn@users.noreply.github.com>
2020-12-29 17:25:50 +0100

    solved

---
+++ romans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+typedef long long ll;
+
+using namespace std;
+
+int main(){
+    double n;
+    
+    cin >> n;
+    
+    cout << ll(round(n*1087.7626)) << endl;
+    
+}
