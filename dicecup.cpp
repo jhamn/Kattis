798bbddca72fd94ebb648a31d76f323e6b38896e
jhamn <36033772+jhamn@users.noreply.github.com>
2020-12-29 17:26:40 +0100

    solved

---
+++ dicecup.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    
+    if(b < a) swap(a,b);
+    
+    for(int i = a + 1; i < b + 2; i++){
+        cout << i << endl;
+    }
+    
+}
