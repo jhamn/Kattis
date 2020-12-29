d3b4250e3011b193cb272c65c026933efc74ebe6
jhamn <36033772+jhamn@users.noreply.github.com>
2020-12-29 17:25:21 +0100

    solved

---
+++ filip.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    
+    cin >> a >> b;
+    
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    if(a < b) cout << b << endl;
+    else cout << a << endl;
+    
+}
