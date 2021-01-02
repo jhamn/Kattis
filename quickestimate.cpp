1a952804112747e08c844fea29fff465e2754581
jhamn <36033772+jhamn@users.noreply.github.com>
2021-01-02 12:14:51 +0100

    solved

---
+++ quickestimate.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+
+    ll n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        string s;
+        cin >> s;
+        cout << s.length() << endl;
+    }
+}
