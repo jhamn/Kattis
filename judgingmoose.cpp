be9ae54b8106d88e49eb3f24e37c34910d322647
jhamn <36033772+jhamn@users.noreply.github.com>
2021-01-02 12:27:02 +0100

    solved

---
+++ judgingmoose.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int l, r;
+    cin >> l >> r;
+
+    if(l == 0 && r == 0){
+        cout << "Not a moose" << endl;
+    }
+    else if(l == r){
+        cout << "Even " << 2*max(l,r) << endl;
+    }
+    else cout << "Odd " << 2*max(l,r) << endl;
+
+}
