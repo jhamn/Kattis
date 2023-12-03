b0b802a62cafb84220cba74cc38cbb7fa24f2f6c
Josef Hamnert <josef.hamnert@gmail.com>
2023-12-03 15:26:37 +0100

    solved

---
+++ class_field_trip.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string a, b;
+    cin >> a >> b;
+    
+    a += b;
+    sort(a.begin(), a.end());
+    cout << a;
+
+    return 0;
+}
